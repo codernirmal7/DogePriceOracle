1127dd7d9a2efceb155345694fc3493429ec358a
Nirmal Bam <codernirmalbam@gmail.com>
2025-01-21 13:03:54 +0545

    Add automatic hitting price/update endpoint

--- DogePriceOracle/src/index.js
+++ DogePriceOracle/src/index.js
@@ -1,8 +1,20 @@
 import app from "./app.js";
 import "dotenv/config";
+import cron from "node-cron";
+import axios from "axios";
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+cron.schedule("0 0 * * *", async () => {
+  try {
+    console.log("Hitting /update endpoint...");
+    const response = await axios.get("https://dogepriceoracle.onrender.com/price/update");
+    console.log("Response from /update:", response.data);
+  } catch (error) {
+    console.error("Error hitting /update endpoint:", error.message);
+  }
 });
